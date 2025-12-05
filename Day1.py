8e690ab5d61cf73bd728a875c1f5b4d1e67c631e
Justin <justinr32274@gmail.com>
2025-12-05 22:42:48 +0530

    Day 2 completed successfully

---
+++ Day1.py
@@ -0,0 +1,19 @@
+print("HI")
+print(5)
+
+
+
+# This is a comment
+
+'''
+print("HI")
+print(5)
+
+print("Hello world \n hey there")
+
+This is a multiline comment
+'''
+# escape sequence
+print("Hello world \t hey there") # tab
+print("Hello world \n hey there") # new line
+print("Hello \"world \" hey there")#double quotes
---
+++ Day2.py
@@ -0,0 +1,35 @@
+#DATATYPES AND VARIABLES
+# need datatypes to do operations
+
+a=1
+print(a) #int type
+b="Justin"
+print(b) #string
+c=True #boolean
+
+print(type(a)) # used to see datatype
+#Operators
+
+'''
+4+5
+7-2
+6*5
+9/2
+9//6
+7%5
+5**3
+'''
+
+#Building a calculator
+
+a=25
+b=4.8
+print("a+b =",a+b)
+print("a/b =",a/b)
+print("a-b =",a-b)
+print("a*b =",a*b)
+print("a//b =",a//b)
+print("a%b =",a%b)
+print("a**b =",a**b)
+print("Congrats you did it!!")
+
