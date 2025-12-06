3a73c24dab68e5b6d098d1ad0d31926e6cdf1d1c
Justin <justinr32274@gmail.com>
2025-12-06 22:56:53 +0530

    Day 3 completed successfully

---
+++ Day3.py
@@ -0,0 +1,33 @@
+#Typecasting in python
+a='1'
+b='2'
+print(a+b) #strings concantinate
+print(int(a)+int(b))# coverted to integer
+
+"""
+Two type of typecasting
+
+1.Implicit
+2.Explicit
+
+"""
+a=5
+b=3.4
+print(a+b) # implicit as the result is converted to float automatically
+
+#Taking user inputs
+a=input ("Enter your name:")
+#input by default takes string
+a=int(input ("Enter your age:"))
+
+
+#STRINGS
+
+a="apple"
+
+b=''' hi there
+my name is
+justin'''
+
+print(a,b)
+print(a[1])# Indexing in string
