4aa9d0caab232134dd29667088b8c051fc89fed2
Justin <justinr32274@gmail.com>
2025-12-07 23:08:11 +0530

    Day 4 completed successfully

---
+++ Day4.py
@@ -0,0 +1,16 @@
+#SLICING
+a='Justin'
+print(a[0:4])
+print(a[:4]) #same as before
+print(a[0:-3])#negative indexing
+#len function
+print(len(a))
+#String methods
+#strings are immutable
+
+s='Justin !!!!!!!!!!'
+print(s.upper())
+print(s.lower())
+print(s.rstrip("!"))
+print(s.replace("u","k"))
+print(s.split(" "))
